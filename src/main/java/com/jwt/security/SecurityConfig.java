43eb965dc0166067af392c91679cab3617c7fefa
krishna <krishna.sk7991@gmail.com>
2021-12-28 12:50:44 +0530

    implemented Enpoint for "/" and issued pemitAll

--- src/main/java/com/jwt/security/SecurityConfig.java
+++ src/main/java/com/jwt/security/SecurityConfig.java
@@ -1,7 +1,5 @@
 package com.jwt.security;
 
-import com.jwt.service.UserServiceImpl;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,12 +7,15 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.jwt.service.UserServiceImpl;
 
 @Configuration
 @EnableWebSecurity
@@ -56,7 +57,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable()
 				.authorizeRequests()
-				.antMatchers("/user/login", "/user/one").permitAll()
+				.antMatchers("/","/user/login", "/user/one").permitAll()
 				// .antMatchers("/student/**").hasRole(ROLES.STUDENT.name())
 				// .antMatchers("/employee/**").hasRole(ROLES.EMPLOYEE.name())
 				// .antMatchers("/admin/**").hasRole(ROLES.ADMIN.name())
