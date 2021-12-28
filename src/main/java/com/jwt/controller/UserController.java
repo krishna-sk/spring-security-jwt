2abe19be4eac36afa3fb84029e3b4005c18d0d43
krishna <krishna.sk7991@gmail.com>
2021-12-28 14:52:39 +0530

    Operation Summary & ApiResponses

--- src/main/java/com/jwt/controller/UserController.java
+++ src/main/java/com/jwt/controller/UserController.java
@@ -5,14 +5,6 @@ import java.util.stream.Collectors;
 
 import javax.validation.Valid;
 
-import com.jwt.entity.User;
-import com.jwt.exception.UserException;
-import com.jwt.model.UserModel;
-import com.jwt.request.UserRequest;
-import com.jwt.response.UserResponse;
-import com.jwt.service.IUserService;
-import com.jwt.util.JwtUtil;
-
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
@@ -26,6 +18,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.jwt.entity.User;
+import com.jwt.exception.ErrorMessage;
+import com.jwt.exception.UserException;
+import com.jwt.model.UserModel;
+import com.jwt.request.UserRequest;
+import com.jwt.response.UserResponse;
+import com.jwt.service.IUserService;
+import com.jwt.util.JwtUtil;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.extern.slf4j.Slf4j;
 
 @RestController
@@ -45,6 +52,11 @@ public class UserController {
 	@Autowired
 	private AuthenticationManager authenticationManager;
 
+	@Operation(summary = "Get a User by username")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "Found the user", content = {
+					@Content(mediaType = "application/json", schema = @Schema(implementation = UserModel.class)) }),
+			@ApiResponse(responseCode = "404", description = "User not found", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class))) })
 	@GetMapping(value = "/", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<UserModel> getUser(@RequestParam String username) {
 		if (!userService.userExistsByUsername(username)) {
@@ -55,6 +67,9 @@ public class UserController {
 		return ResponseEntity.ok(returnUserModel);
 	}
 
+	@Operation(summary = "Get a User for login")
+	@ApiResponses(value = { @ApiResponse(responseCode = "200", description = "Found the user", content = {
+			@Content(mediaType = "application/json", schema = @Schema(implementation = UserModel.class)) }) })
 	@GetMapping(value = "/one", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<UserModel> getOneUser() {
 		List<User> allUsers = userService.getAllUsers();
@@ -65,6 +80,11 @@ public class UserController {
 		return ResponseEntity.ok(returnUserModel);
 	}
 
+	@Operation(summary = "Get all Users")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "Found all users", content = {
+					@Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = UserModel.class))) }),
+			@ApiResponse(responseCode = "404", description = "Users not found", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class))) })
 	@GetMapping(value = "/all", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<List<UserModel>> getAllUsers() {
 		List<User> allUsers = userService.getAllUsers();
@@ -76,6 +96,11 @@ public class UserController {
 		return ResponseEntity.ok(listOfUserModels);
 	}
 
+	@Operation(summary = "Create User")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "Successfully Created a User", content = {
+					@Content(mediaType = "application/json", schema = @Schema(implementation = UserModel.class)) }),
+			@ApiResponse(responseCode = "400", description = "Cannot Create a User as Username is already taken !!!", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class))) })
 	@PostMapping(value = "/save", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<UserModel> saveUser(@Valid @RequestBody UserModel userModel) {
 		if (userService.userExistsByUsername(userModel.getUsername())) {
@@ -90,6 +115,10 @@ public class UserController {
 		return ResponseEntity.ok(returnUserModel);
 	}
 
+	@Operation(summary = "Login")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "Successfully Logged in", content = @Content(mediaType = "application/json", schema = @Schema(implementation = UserResponse.class))),
+			@ApiResponse(responseCode = "400", description = "Invalid Credentails", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class))) })
 	@PostMapping(value = "/login", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<UserResponse> loginUser(@Valid @RequestBody UserRequest userRequest) {
 
--- src/main/java/com/jwt/request/UserRequest.java
+++ src/main/java/com/jwt/request/UserRequest.java
@@ -2,6 +2,7 @@ package com.jwt.request;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -12,10 +13,13 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class UserRequest {
 
-    @NotNull(message = "username cannot be null ")
-    @NotBlank(message = "username cannot be empty")
-    private String username;
+	@NotNull(message = "username cannot be null ")
+	@NotBlank(message = "username cannot be empty")
+	private String username;
 
-    private String password;
-    
+	@Size(min = 8, max = 20, message = "password Must be between 8 and 20 Characters")
+	@NotNull(message = "password cannot be null ")
+	@NotBlank(message = "password cannot be empty")
+	private String password;
+
 }
