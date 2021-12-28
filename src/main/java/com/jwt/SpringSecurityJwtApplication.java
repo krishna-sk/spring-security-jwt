32951cd9884c012cca897d223cf5f02d46a50408
krishna <krishna.sk7991@gmail.com>
2021-12-28 12:49:28 +0530

    improved Code Readability. Sonar Lint Issues

--- src/main/java/com/jwt/SpringSecurityJwtApplication.java
+++ src/main/java/com/jwt/SpringSecurityJwtApplication.java
@@ -3,11 +3,6 @@ package com.jwt;
 import java.util.Arrays;
 import java.util.HashSet;
 
-import com.jwt.entity.User;
-import com.jwt.enums.PERMISSIONS;
-import com.jwt.enums.ROLES;
-import com.jwt.repository.UserRepository;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -15,11 +10,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import com.jwt.entity.User;
+import com.jwt.enums.PERMISSIONS;
+import com.jwt.enums.ROLES;
+import com.jwt.repository.UserRepository;
+
 @SpringBootApplication
 public class SpringSecurityJwtApplication {
 
 	@Autowired
 	private PasswordEncoder passwordEncoder;
+
+	private static final String PASSWORD = "password";
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringSecurityJwtApplication.class, args);
@@ -28,38 +30,31 @@ public class SpringSecurityJwtApplication {
 	@Bean
 	CommandLineRunner run(UserRepository userRepository) {
 		return args -> {
-			User admin_read_write = User.builder().name("admin").username("admin_read_write")
-					.password(passwordEncoder.encode("password"))
-					.roles(new HashSet<>(Arrays.asList(ROLES.ADMIN)))
+			User adminReadWrite = User.builder().name("admin").username("admin_read_write")
+					.password(passwordEncoder.encode(PASSWORD)).roles(new HashSet<>(Arrays.asList(ROLES.ADMIN)))
 					.permissions(new HashSet<>(Arrays.asList(PERMISSIONS.READ, PERMISSIONS.WRITE))).build();
-			User admin_update_delete = User.builder().name("admin").username("admin_update_delete")
-					.password(passwordEncoder.encode("password"))
-					.roles(new HashSet<>(Arrays.asList(ROLES.ADMIN)))
+			User adminUpdateDelete = User.builder().name("admin").username("admin_update_delete")
+					.password(passwordEncoder.encode(PASSWORD)).roles(new HashSet<>(Arrays.asList(ROLES.ADMIN)))
 					.permissions(new HashSet<>(Arrays.asList(PERMISSIONS.UPDATE, PERMISSIONS.DELETE))).build();
 
-			User employee_read_write = User.builder().name("employee").username("employee_read_write")
-					.password(passwordEncoder.encode("password"))
-					.roles(new HashSet<>(Arrays.asList(ROLES.EMPLOYEE)))
+			User employeeReadWrite = User.builder().name("employee").username("employee_read_write")
+					.password(passwordEncoder.encode(PASSWORD)).roles(new HashSet<>(Arrays.asList(ROLES.EMPLOYEE)))
 					.permissions(new HashSet<>(Arrays.asList(PERMISSIONS.READ, PERMISSIONS.WRITE))).build();
 
-			User employee_update_delete = User.builder().name("employee").username("employee_update_delete")
-					.password(passwordEncoder.encode("password"))
-					.roles(new HashSet<>(Arrays.asList(ROLES.EMPLOYEE)))
+			User employeeUpdateDelete = User.builder().name("employee").username("employee_update_delete")
+					.password(passwordEncoder.encode(PASSWORD)).roles(new HashSet<>(Arrays.asList(ROLES.EMPLOYEE)))
 					.permissions(new HashSet<>(Arrays.asList(PERMISSIONS.UPDATE, PERMISSIONS.DELETE))).build();
 
-			User student_read_write = User.builder().name("krishna").username("student_read_write")
-					.password(passwordEncoder.encode("password"))
-					.roles(new HashSet<>(Arrays.asList(ROLES.STUDENT)))
+			User studentReadWrite = User.builder().name("student").username("student_read_write")
+					.password(passwordEncoder.encode(PASSWORD)).roles(new HashSet<>(Arrays.asList(ROLES.STUDENT)))
 					.permissions(new HashSet<>(Arrays.asList(PERMISSIONS.READ, PERMISSIONS.WRITE))).build();
 
-			User student_update_delete = User.builder().name("krishna").username("student_update_delete")
-					.password(passwordEncoder.encode("password"))
-					.roles(new HashSet<>(Arrays.asList(ROLES.STUDENT)))
+			User studentUpdateDelete = User.builder().name("student").username("student_update_delete")
+					.password(passwordEncoder.encode(PASSWORD)).roles(new HashSet<>(Arrays.asList(ROLES.STUDENT)))
 					.permissions(new HashSet<>(Arrays.asList(PERMISSIONS.UPDATE, PERMISSIONS.DELETE))).build();
 
-			userRepository.saveAll(
-					Arrays.asList(admin_read_write, admin_update_delete, employee_read_write, employee_update_delete,
-							student_read_write, student_update_delete));
+			userRepository.saveAll(Arrays.asList(adminReadWrite, adminUpdateDelete, employeeReadWrite,
+					employeeUpdateDelete, studentReadWrite, studentUpdateDelete));
 		};
 	}
 }
